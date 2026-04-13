faf96dd908c830d098aa74dd0dde341f89cf4259
20213tn098 <20213tn098@utez.edu.mx>
2026-04-13 11:27:08 -0600

    proyecto refactorizado fix

--- ecommerce_identity_service/pedidos/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from .views import PedidoCreateView, PedidoDetailView, PedidoStatusUpdateView
-
-urlpatterns = [
-    path('', PedidoCreateView.as_view(), name='pedido-create'),
-    path('<int:id>/', PedidoDetailView.as_view(), name='pedido-detail'),
-    path('<int:id>/status/', PedidoStatusUpdateView.as_view(), name='pedido-status-update'),
-]
--- ecommerce_identity_service/usuarios/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import RegistroView, LoginView, PerfilView
-
-urlpatterns = [
-    path('register/',          RegistroView.as_view(), name='registro'),
-    path('login/',             LoginView.as_view(),    name='login'),
-    path('<int:id>/profile/',  PerfilView.as_view(),   name='perfil'),
-]
